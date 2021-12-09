aac53f970be8788531967955a81b393a14210104
agres <ale.agresta46@gmail.com>
2021-12-09 10:16:56 +0100

    bug fix

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
 const outputHtml = document.getElementById('output');
 const lenght = prompt('Quanti km è lungo il viaggio?');
 const age = prompt('Quanti anni hai?');
-let price = 0.21 * lenght;
+const pricePerKm = 0.21;
+const salesjunior = 0.2;
+const salessenior = 0.4;
+
+
+
+let price = pricePerKm * lenght;
+console.log(price);
 
 if (age < 18) {
-    outputHtml.innerHTML = price * 0.2;
-} else if (age > 65) {
-    outputHtml.innerHTML = price * 0.4;
-}  else {
-    outputHtml.innerHTML = price;
+    const discount = price * salesjunior;
+    price = price - discount;
+} 
+
+if (age >= 65) {
+    const discount = price * salessenior;
+    price = price - discount;
 }
+ 
+
+
+console.log(price)
+
+price = price.toFixed(2);
+
+console.log(price);
+
+outputHtml.innerHTML = `il prezzo è ${price}€`
